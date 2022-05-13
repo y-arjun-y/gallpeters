2a2bc005ecbe2f824d36d02d52362630fe9939a5
y-arjun-y <arjyadav7@gmail.com>
2022-05-13 12:20:19 +0400

    changed assets and scripts code

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const port = 3000;
 
-app.use("/assets", express.static("assets"));
-app.use("/scripts", express.static("scripts"));
+app.use("/assets", express.static(path.join(__dirname + "/assets")));
+app.use("/scripts", express.static(path.join(__dirname + "/scripts")));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
